715b3d47c755ecc9a19f146f18c241fab1044734
Spendylove Owusu <spendy00@Spendyloves-MacBook-Pro-2.local>
2022-12-16 21:05:27 -0500

    First commit

--- lab2.c
+++ lab2.c
@@ -1,24 +1,8 @@
-#include <stdio.h>
-#include <pthread.h>
-#include <stdlib.h>
+//
+//  lab2.c
+//  
+//
+//  Created by Spendylove Owusu on 12/16/22.
+//
+
 #include "lab2.h"
-
-
-int** read_board_from_file(char* filename){
-    FILE *fp = NULL;
-    int** board = NULL;
-
-    // replace this comment with your code
-
-    return board;
-}
-
-
-int is_board_valid(){
-    pthread_t* tid;  /* the thread identifiers */
-    pthread_attr_t attr;
-    param_struct* parameter;
-    
-    // replace this comment with your code
-    
-}
